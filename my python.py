b0afc6acac9ef2ac69801a762f3326f2804d66a0
juasuglo22 <juasuglorashida@gmail.com>
2020-07-03 04:35:03 -0700

    initial commit

---
+++ my python.py
@@ -0,0 +1,13 @@
+myname ="Rashida"
+mynumber = 100
+mybool = True
+
+
+
+def full_name(first, last):
+    return first +" " +last
+
+
+    full_name("Juasuglo", "Rashida")
+
+    #comments in python
